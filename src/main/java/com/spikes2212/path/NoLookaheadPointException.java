5e8eccf8fe7c27a292b3db896f9974825a2f1e35
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-12-09 16:57:25 +0200

    added NoLookaheadPointException

---
+++ src/main/java/com/spikes2212/path/NoLookaheadPointException.java
@@ -0,0 +1,12 @@
+package com.spikes2212.path;
+
+public class NoLookaheadPointException extends Exception {
+
+    public NoLookaheadPointException() {
+        super();
+    }
+
+    public NoLookaheadPointException(String message) {
+        super(message);
+    }
+}
