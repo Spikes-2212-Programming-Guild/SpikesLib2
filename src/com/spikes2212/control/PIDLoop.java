e32e0772544b9e54d917a06e97014f5fdc84fedd
Ergoold <eran1gold@gmail.com>
2019-12-09 14:24:12 +0200

    re-added `PIDLoop`

--- src/com/spikes2212/control/PIDLoop.java
+++ src/com/spikes2212/control/PIDLoop.java
@@ -1,4 +1,32 @@
 package com.spikes2212.control;
 
+/**
+ * An interface for PID loops.
+ *
+ * @author Eran Goldstein
+ */
 public interface PIDLoop {
+    /**
+     * Starts the PID loop.
+     */
+    void enable();
+
+    /**
+     * Stops the PID loop.
+     */
+    void disable();
+
+    /**
+     * Sets the setpoint of the loop.
+     *
+     * @param setpoint the new setpoint of the loop
+     */
+    void setSetpoint(double setpoint);
+
+    /**
+     * Check whether the loop is currently within the target range.
+     *
+     * @return true when within target range, false otherwise
+     */
+    boolean onTarget();
 }
