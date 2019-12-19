de17e51e43a1c7b88a9f5ae839cd90a50337d360
YuvalLevy1 <yuval124levy1@gmail.com>
2019-12-19 18:34:18 +0200

    reformatted file

--- src/main/java/com/spikes2212/control/JoinedPID.java
+++ src/main/java/com/spikes2212/control/JoinedPID.java
@@ -3,38 +3,39 @@ package com.spikes2212.control;
 import java.util.Arrays;
 import java.util.List;
 
-public class JoinedPID implements  PIDLoop{
+public class JoinedPID implements PIDLoop {
 
     private List<PIDLoop> pidLoops;
 
-    public JoinedPID(PIDLoop... pidLoops){
+    public JoinedPID(PIDLoop... pidLoops) {
         this.pidLoops.addAll(Arrays.asList(pidLoops));
     }
+
     @Override
     public void enable() {
-        for(PIDLoop pidLoop : pidLoops){
+        for (PIDLoop pidLoop : pidLoops) {
             pidLoop.enable();
         }
     }
 
     @Override
     public void disable() {
-        for(PIDLoop pidLoop : pidLoops){
+        for (PIDLoop pidLoop : pidLoops) {
             pidLoop.disable();
         }
     }
 
     @Override
     public void update() {
-        for(PIDLoop pidLoop : pidLoops){
+        for (PIDLoop pidLoop : pidLoops) {
             pidLoop.update();
         }
     }
 
     @Override
     public boolean onTarget() {
-        for(PIDLoop pidLoop : pidLoops){
-            if(!pidLoop.onTarget())
+        for (PIDLoop pidLoop : pidLoops) {
+            if (!pidLoop.onTarget())
                 return false;
         }
         return true;
