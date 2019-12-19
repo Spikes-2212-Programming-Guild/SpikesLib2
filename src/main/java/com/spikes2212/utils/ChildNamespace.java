23523da0ca9a0fd78dcf42db4518a791e354a305
talsi <tal.sitton04@gmail.com>
2019-12-19 21:25:33 +0200

    Implemented Namespace classes.

--- src/main/java/com/spikes2212/utils/ChildNamespace.java
+++ src/main/java/com/spikes2212/utils/ChildNamespace.java
@@ -4,71 +4,13 @@ import edu.wpi.first.wpilibj.Sendable;
 
 import java.util.function.Supplier;
 
-public class ChildNamespace implements Namespace {
+public class ChildNamespace extends RootNamespace {
 
-    private String name;
+    private Namespace parent;
 
-    public ChildNamespace(String name) {
-        this.name = name;
+    public ChildNamespace(String name, Namespace parent) {
+        super(name);
+        this.parent = parent;
     }
 
-    @Override
-    public Supplier<Double> addConstantDouble(String name, double value) {
-        return null;
-    }
-
-    @Override
-    public Supplier<Integer> addConstantInt(String name, int value) {
-        return null;
-    }
-
-    @Override
-    public Supplier<String> addConstantString(String name, String value) {
-        return null;
-    }
-
-    @Override
-    public Namespace addChild(String name) {
-        return null;
-    }
-
-    @Override
-    public void putData(String key, Sendable value) {
-
-    }
-
-    @Override
-    public Sendable getSendable(String key) {
-        return null;
-    }
-
-    @Override
-    public void putString(String key, String value) {
-
-    }
-
-    @Override
-    public String getString(String key) {
-        return null;
-    }
-
-    @Override
-    public void putNumber(String key, double value) {
-
-    }
-
-    @Override
-    public double getNumber(String key) {
-        return 0;
-    }
-
-    @Override
-    public void putBoolean(String key, boolean value) {
-
-    }
-
-    @Override
-    public boolean getBoolean(String key) {
-        return false;
-    }
 }
--- src/main/java/com/spikes2212/utils/RootNamespace.java
+++ src/main/java/com/spikes2212/utils/RootNamespace.java
@@ -1,5 +1,6 @@
 package com.spikes2212.utils;
 
+import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.Sendable;
 
 import java.util.function.Supplier;
@@ -7,68 +8,90 @@ import java.util.function.Supplier;
 public class RootNamespace implements Namespace {
 
     private String name;
+    private NetworkTable table;
 
     public RootNamespace(String name) {
         this.name = name;
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        this.table = inst.getTable(this.name);
     }
 
     @Override
     public Supplier<Double> addConstantDouble(String name, double value) {
-        return null;
+        NetworkTableEntry entry =this.table.getEntry(name);
+        entry.setDouble(value);
+        return () -> value;
     }
 
     @Override
     public Supplier<Integer> addConstantInt(String name, int value) {
-        return null;
+        NetworkTableEntry entry =this.table.getEntry(name);
+        entry.setNumber(value);
+        return () -> value;
     }
 
     @Override
     public Supplier<String> addConstantString(String name, String value) {
-        return null;
+        NetworkTableEntry entry =this.table.getEntry(name);
+        entry.setString(value);
+        return () ->value;
     }
 
     @Override
     public Namespace addChild(String name) {
-        return new ChildNamespace(this.name + '/' + name);
+
+        return new ChildNamespace(name, this);
     }
 
     @Override
-    public void putData(String key, Sendable value) {
-
+    public void putData(String key, Sendable value) { //todo
+        NetworkTableEntry entry =this.table.getEntry(key);
+        entry.setValue(value);
     }
 
     @Override
-    public Sendable getSendable(String key) {
-        return null;
+    public Sendable getSendable(String key) { //todo
+        NetworkTableEntry entry =this.table.getEntry(key);
+        NetworkTableValue value =entry.getValue();
+        return (Sendable) value.getValue();
     }
 
     @Override
     public void putString(String key, String value) {
-
+        NetworkTableEntry entry =this.table.getEntry(key);
+        entry.setString(value);
     }
 
     @Override
     public String getString(String key) {
-        return null;
+        NetworkTableEntry entry =this.table.getEntry(key);
+        NetworkTableValue value=entry.getValue();
+        return value.getString();
     }
 
     @Override
     public void putNumber(String key, double value) {
-
+        NetworkTableEntry entry =this.table.getEntry(key);
+        entry.setDouble(value);
     }
 
     @Override
     public double getNumber(String key) {
-        return 0;
+        NetworkTableEntry entry =this.table.getEntry(key);
+        NetworkTableValue value =entry.getValue();
+        return value.getDouble();
     }
 
     @Override
     public void putBoolean(String key, boolean value) {
-
+        NetworkTableEntry entry =this.table.getEntry(key);
+        entry.setBoolean(value);
     }
 
     @Override
     public boolean getBoolean(String key) {
-        return false;
+        NetworkTableEntry entry =this.table.getEntry(key);
+        NetworkTableValue value = entry.getValue();
+        return value.getBoolean();
     }
 }
