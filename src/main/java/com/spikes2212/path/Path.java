959f3742415875ec15b772f6b5b3284a5101ca73
tuval88 <ttuval6677@gamil.com>
2020-01-08 17:23:07 +0200

    return of the NaN check

--- src/main/java/com/spikes2212/path/Path.java
+++ src/main/java/com/spikes2212/path/Path.java
@@ -122,7 +122,10 @@ public class Path {
             double b = 0.5 * (x2*x2 - 2*2*k1 + y2*y2 - x3*x3 + 2*x3*k1 - y3*y3) / (x3*k2 - y3 + y2 - x2*k2);
             double a = k1 - k2 * b;
             double r = Math.sqrt((x1-a)*(x1-a) + (y1-b)*(y1-b));
-            points.get(i).setCurvature(1/r);
+            if(Double.isNaN(r))
+                points.get(i).setCurvature(0);
+            else
+                points.get(i).setCurvature(1/r);
         }
     }
 
