48b409dbe9e344af38fd7b755054131906ed39d3
tuval88 <ttuval6677@gamil.com>
2020-01-08 19:00:09 +0200

    fixed NaN problem (for real this time)

--- src/main/java/com/spikes2212/path/Path.java
+++ src/main/java/com/spikes2212/path/Path.java
@@ -62,6 +62,7 @@ public class Path {
         for (int i = 0; i < points.size() - 1; i++) {
             double slopeAngle = Math.atan2(points.get(i+1).getY() - points.get(i).getY(),
                         points.get(i+1).getX() - points.get(i).getX());
+            if(Double.isNaN(slopeAngle)) slopeAngle = 0;
             double xOffset = distance * Math.cos(slopeAngle);
             double yOffset = distance * Math.sin(slopeAngle);
             double tempX = points.get(i).getX() + xOffset, tempY = points.get(i).getY() + yOffset;
@@ -122,7 +123,6 @@ public class Path {
             double b = 0.5 * (x2*x2 - 2*x2*k1 + y2*y2 - x3*x3 + 2*x3*k1 - y3*y3) / (x3*k2 - y3 + y2 - x2*k2);
             double a = k1 - k2 * b;
             double r = Math.sqrt((x1-a)*(x1-a) + (y1-b)*(y1-b));
-            if (r == 0 || Double.isNaN(r)) throw new IllegalArgumentException("r in calcCurv is NaN");
             points.get(i).setCurvature(1/r);
         }
     }
