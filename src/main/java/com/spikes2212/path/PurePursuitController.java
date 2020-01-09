883808effc32c705d788a4ced7eb1ab404cadae2
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-09 21:17:20 +0200

    fixed yaw handling in path curvature

--- src/main/java/com/spikes2212/path/PurePursuitController.java
+++ src/main/java/com/spikes2212/path/PurePursuitController.java
@@ -98,9 +98,9 @@ public class PurePursuitController {
         } catch (LookaheadPointNotFoundException lpnfe) {
             throw new LookaheadPointNotFoundException(lpnfe);
         }
-        double yaw = handler.getYaw();
-        double slope = Math.tan(Math.PI/2 - yaw);
-        double freeTerm = -slope * robot.getX() - robot.getY();
+        double yaw = Math.toRadians(90 - handler.getYaw());
+        double slope = Math.tan(yaw);
+        double freeTerm = slope * robot.getX() - robot.getY();
         double x = Math.abs(-slope * lookahead.getX() + lookahead.getY() + freeTerm) /
                 Math.sqrt(slope * slope + 1); //distance between lookahead point and robot line
         double side = Math.sin(yaw) * (lookahead.getX() - robot.getX()) -
