2e94a76e04d780b1827950ec2a1558a6560af48f
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-11 17:13:27 +0200

    removed unnecessary import

--- src/main/java/com/spikes2212/path/Path.java
+++ src/main/java/com/spikes2212/path/Path.java
@@ -2,7 +2,6 @@ package com.spikes2212.path;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
