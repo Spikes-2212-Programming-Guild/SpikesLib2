e6a3afa02802f9133e392e435ed2c6d010891cd5
talsi <tal.sitton04@gmail.com>
2020-01-11 11:29:48 +0200

    added separator in the default constructor and changed the separator in all the functions

--- src/main/java/com/spikes2212/utils/ChildNamespace.java
+++ src/main/java/com/spikes2212/utils/ChildNamespace.java
@@ -9,64 +9,66 @@ import java.util.function.Supplier;
 public class ChildNamespace extends RootNamespace {
 
     private Namespace parent;
+    private String separator;
 
     public ChildNamespace(String name, Namespace parent) {
         super(name);
         this.parent = parent;
+        separator="/";
     }
     @Override
     public Supplier<Double> addConstantDouble(String name, double value) {
-        return parent.addConstantDouble(this.name + " " + name,value);
+        return parent.addConstantDouble(this.name + separator + name,value);
     }
 
     @Override
     public Supplier<Integer> addConstantInt(String name, int value) {
-        return parent.addConstantInt(this.name + " " + name,value);
+        return parent.addConstantInt(this.name + separator + name,value);
     }
 
     @Override
     public Supplier<String> addConstantString(String name, String value) {
-        return parent.addConstantString(this.name + " " + name,value);
+        return parent.addConstantString(this.name +separator+ name,value);
     }
 
     @Override
     public void putData(String key, Sendable value) {
-        parent.putData ( name + " " + key,value);
+        parent.putData ( name + separator + key,value);
     }
 
     @Override
     public Sendable getSendable(String key) {
-        return parent.getSendable(name+""+key);
+        return parent.getSendable(name+separator+key);
     }
 
     @Override
     public void putString(String key, String value) {
-        parent.putString(name+ "" + key,value);
+        parent.putString(name+ separator + key,value);
     }
 
     @Override
     public String getString(String key) {
-        return parent.getString(name+ "" + key);
+        return parent.getString(name+ separator + key);
     }
 
     @Override
     public void putNumber(String key, double value) {
-        parent.putNumber(name+ "" + key,value);
+        parent.putNumber(name+ separator + key,value);
     }
 
     @Override
     public double getNumber(String key) {
-        return parent.getNumber(name+ "" +key);
+        return parent.getNumber(name+ separator +key);
     }
 
     @Override
     public void putBoolean(String key, boolean value) {
-        parent.putBoolean(name+ "" +key,value);
+        parent.putBoolean(name+ separator +key,value);
     }
 
     @Override
     public boolean getBoolean(String key) {
-       return parent.getBoolean(name+ "" +key);
+       return parent.getBoolean(name+ separator +key);
     }
 
 }
