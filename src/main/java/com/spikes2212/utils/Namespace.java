5571aad3fa88f74ddf2c25699a26e9426c04178c
talsi <tal.sitton04@gmail.com>
2020-01-18 14:54:58 +0200

    added default put functions

--- src/main/java/com/spikes2212/utils/Namespace.java
+++ src/main/java/com/spikes2212/utils/Namespace.java
@@ -20,13 +20,26 @@ public interface Namespace {
 
     void putString(String key, Supplier<String> value);
 
+    default void putString(String key, String value) {
+        this.putString(key, () -> value);
+    }
+
     String getString(String key);
 
     void putNumber(String key, Supplier<? extends Number> value);
 
+    default void putNumber(String key, Number number) {
+        this.putNumber(key, () -> number);
+    }
+
+
     double getNumber(String key);
 
     void putBoolean(String key, Supplier<Boolean> value);
+
+    default void putBoolean(String key, boolean value) {
+        this.putBoolean(key, () -> value);
+    }
 
     boolean getBoolean(String key);
 }
