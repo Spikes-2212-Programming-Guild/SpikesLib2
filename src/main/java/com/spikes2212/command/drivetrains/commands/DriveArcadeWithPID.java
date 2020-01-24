3230efd2b62f915a0da865e3a0b5e9bac8d8e44c
Ergoold <eran1gold@gmail.com>
2020-01-24 13:30:08 +0200

    adapted `DriveArcadeWithPID` to new SL PID

--- src/main/java/com/spikes2212/command/drivetrains/commands/DriveArcadeWithPID.java
+++ src/main/java/com/spikes2212/command/drivetrains/commands/DriveArcadeWithPID.java
@@ -1,7 +1,9 @@
 package com.spikes2212.command.drivetrains.commands;
 
 import com.spikes2212.command.drivetrains.TankDrivetrain;
-import com.spikes2212.control.PIDLoop;
+import com.spikes2212.control.PIDSettings;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import java.util.function.Supplier;
@@ -10,32 +12,28 @@ import java.util.function.Supplier;
 public class DriveArcadeWithPID extends CommandBase {
 
     private final TankDrivetrain drivetrain;
-    private final PIDLoop pidLoop;
+    private PIDSettings pidSettings;
+    private PIDController pidController;
+    private Supplier<Double> source;
+    private double lastTimeNotOnTarget;
     private Supplier<Double> setpoint;
+    private Supplier<Double> moveValue;
 
-    /**
-     * @param drivetrain is the {@link TankDrivetrain} the command moves.
-     * @param pidLoop is the {@link PIDLoop} that calculates and sets the speed to the drivetrain.
-     */
-
-    public DriveArcadeWithPID(TankDrivetrain drivetrain, PIDLoop pidLoop, Supplier<Double> setpoint) {
+    public DriveArcadeWithPID(TankDrivetrain drivetrain, PIDSettings pidSettings, Supplier<Double> source,
+                              Supplier<Double> setpoint, Supplier<Double> moveValue) {
         addRequirements(drivetrain);
         this.drivetrain = drivetrain;
         this.setpoint = setpoint;
-        this.pidLoop = pidLoop;
-        this.pidLoop.setSetpoint(setpoint.get());
+        this.pidSettings = pidSettings;
+        this.source = source;
+        this.moveValue = moveValue;
+        this.pidController = new PIDController(pidSettings.getkP(), pidSettings.getkI(), pidSettings.getkD());
+        this.pidController.setSetpoint(setpoint.get());
     }
 
-    public DriveArcadeWithPID(TankDrivetrain drivetrain, PIDLoop pidLoop, double setpoint){
-        this(drivetrain, pidLoop, () -> setpoint);
-    }
-
-    /**
-     * starts the given PIDLoop.
-     */
-    @Override
-    public void initialize() {
-        pidLoop.enable();
+    public DriveArcadeWithPID(TankDrivetrain drivetrain, PIDSettings pidSettings, Supplier<Double> source,
+                              double setpoint, double moveValue) {
+        this(drivetrain, pidSettings, source, () -> setpoint, () -> moveValue);
     }
 
     /**
@@ -43,19 +41,23 @@ public class DriveArcadeWithPID extends CommandBase {
      */
     @Override
     public void execute() {
-        pidLoop.setSetpoint(setpoint.get());
-        pidLoop.update();
+        pidController.setSetpoint(setpoint.get());
+        pidController.setTolerance(pidSettings.getTolerance());
+        pidController.setPID(pidSettings.getkP(), pidSettings.getkI(), pidSettings.getkD());
+        drivetrain.arcadeDrive(moveValue.get(), pidController.calculate(source.get(), setpoint.get()));
     }
 
     @Override
     public void end(boolean interrupted) {
-        pidLoop.disable();
         drivetrain.stop();
     }
 
     @Override
     public boolean isFinished() {
-        return pidLoop.onTarget();
-    }
+        if(!pidController.atSetpoint()) {
+            lastTimeNotOnTarget = Timer.getFPGATimestamp();
+        }
 
+        return Timer.getFPGATimestamp() - lastTimeNotOnTarget >= pidSettings.getWaitTime();
+    }
 }
