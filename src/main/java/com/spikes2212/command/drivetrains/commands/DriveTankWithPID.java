73575d507bb5ce6dcc53009f3a1eeed28598a258
Ergoold <eran1gold@gmail.com>
2020-01-24 11:43:50 +0200

    update `DriveTankWithPID` with new SL PID

--- src/main/java/com/spikes2212/command/drivetrains/commands/DriveTankWithPID.java
+++ src/main/java/com/spikes2212/command/drivetrains/commands/DriveTankWithPID.java
@@ -1,7 +1,9 @@
 package com.spikes2212.command.drivetrains.commands;
 
 import com.spikes2212.command.drivetrains.TankDrivetrain;
-import com.spikes2212.control.PIDLoop;
+import com.spikes2212.control.PIDSettings;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import java.util.function.Supplier;
@@ -9,41 +11,38 @@ import java.util.function.Supplier;
 public class DriveTankWithPID extends CommandBase {
 
     private final TankDrivetrain drivetrain;
-    private final PIDLoop leftPIDLoop;
-    private final PIDLoop rightPIDLoop;
+    private PIDSettings leftPIDSettings;
+    private PIDSettings rightPIDSettings;
+    private PIDController leftPIDController;
+    private PIDController rightPIDController;
     private Supplier<Double> leftSetpoint;
     private Supplier<Double> rightSetpoint;
+    private Supplier<Double> leftSource;
+    private Supplier<Double> rightSource;
+    private double leftLastTimeNotOnTarget;
+    private double rightLastTimeNotOnTarget;
 
-    /**
-     * @param drivetrain    is the {@link TankDrivetrain} the command moves.
-     * @param rightPIDLoop  is the {@link PIDLoop} that calculates and sets the speed to the drivetrain.
-     * @param rightSetpoint is the setpoint for the PIDLoop.
-     */
-
-    public DriveTankWithPID(TankDrivetrain drivetrain, PIDLoop leftPIDLoop, PIDLoop rightPIDLoop,
-                            Supplier<Double> leftSetpoint, Supplier<Double> rightSetpoint) {
+    public DriveTankWithPID(TankDrivetrain drivetrain, PIDSettings leftPIDSettings, PIDSettings rightPIDSettings, PIDController leftPIDController,
+                            PIDController rightPIDController, Supplier<Double> leftSetpoint, Supplier<Double> rightSetpoint,
+                            Supplier<Double> leftSource, Supplier<Double> rightSource) {
         addRequirements(drivetrain);
         this.drivetrain = drivetrain;
-        this.leftPIDLoop = leftPIDLoop;
-        this.rightPIDLoop = rightPIDLoop;
+        this.leftPIDSettings = leftPIDSettings;
+        this.leftPIDController = leftPIDController;
+        this.rightPIDSettings = rightPIDSettings;
+        this.rightPIDController = rightPIDController;
         this.leftSetpoint = leftSetpoint;
         this.rightSetpoint = rightSetpoint;
-        this.rightPIDLoop.setSetpoint(rightSetpoint.get());
-        this.leftPIDLoop.setSetpoint(leftSetpoint.get());
+        this.leftSource = leftSource;
+        this.rightSetpoint = rightSource;
+        this.leftPIDController.setSetpoint(leftSetpoint.get());
+        this.rightPIDController.setSetpoint(rightSetpoint.get());
     }
 
-    public DriveTankWithPID(TankDrivetrain drivetrain, PIDLoop leftPIDLoop, PIDLoop rightPIDLoop,
-                            double leftSetpoint, double rightSetpoint) {
-        this(drivetrain, rightPIDLoop, leftPIDLoop, () -> rightSetpoint, () -> leftSetpoint);
-    }
-
-    /**
-     * starts the given PIDLoop.
-     */
-    @Override
-    public void initialize() {
-        leftPIDLoop.enable();
-        rightPIDLoop.enable();
+    public DriveTankWithPID(TankDrivetrain drivetrain, PIDSettings leftPIDSettings, PIDController leftPIDController, PIDSettings rightPIDSettings,
+                            PIDController rightPIDController, double leftSetpoint, double rightSetpoint,
+                            Supplier<Double> leftSource, Supplier<Double> rightSource) {
+        this(drivetrain, leftPIDSettings, rightPIDSettings, leftPIDController, rightPIDController, () -> leftSetpoint, () -> rightSetpoint, leftSource, rightSource);
     }
 
     /**
@@ -51,21 +50,32 @@ public class DriveTankWithPID extends CommandBase {
      */
     @Override
     public void execute() {
-        leftPIDLoop.setSetpoint(rightSetpoint.get());
-        rightPIDLoop.setSetpoint(leftSetpoint.get());
-        leftPIDLoop.update();
-        rightPIDLoop.update();
+        leftPIDController.setSetpoint(rightSetpoint.get());
+        rightPIDController.setSetpoint(leftSetpoint.get());
+        leftPIDController.setTolerance(leftPIDSettings.getTolerance());
+        rightPIDController.setTolerance(rightPIDSettings.getTolerance());
+        leftPIDController.setPID(leftPIDSettings.getkP(), leftPIDSettings.getkI(), leftPIDSettings.getkD());
+        rightPIDController.setPID(rightPIDSettings.getkP(), rightPIDSettings.getkI(), rightPIDSettings.getkD());
+        drivetrain.setLeft(leftPIDController.calculate(leftSource.get()));
+        drivetrain.setRight(rightPIDController.calculate(rightSource.get()));
     }
 
     @Override
     public void end(boolean interrupted) {
-        leftPIDLoop.disable();
-        rightPIDLoop.disable();
         drivetrain.stop();
     }
 
     @Override
     public boolean isFinished() {
-        return leftPIDLoop.onTarget() && rightPIDLoop.onTarget();
+        if(!leftPIDController.atSetpoint()) {
+            leftLastTimeNotOnTarget = Timer.getFPGATimestamp();
+        }
+
+        if(!rightPIDController.atSetpoint()) {
+            rightLastTimeNotOnTarget = Timer.getFPGATimestamp();
+        }
+
+        return Timer.getFPGATimestamp() - leftLastTimeNotOnTarget >= leftPIDSettings.getWaitTime()
+                && Timer.getFPGATimestamp() - rightLastTimeNotOnTarget >= rightPIDSettings.getWaitTime();
     }
 }
