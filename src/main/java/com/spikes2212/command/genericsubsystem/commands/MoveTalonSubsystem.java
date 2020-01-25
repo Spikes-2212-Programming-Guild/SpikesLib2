b40da95818b42904c25395029bfda3784e74dca8
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-25 13:04:43 +0200

    Revert "move time handling to `MoveTalonSubsystem`"
    This reverts commit a4bd9ade

--- src/main/java/com/spikes2212/command/genericsubsystem/commands/MoveTalonSubsystem.java
+++ src/main/java/com/spikes2212/command/genericsubsystem/commands/MoveTalonSubsystem.java
@@ -1,7 +1,6 @@
 package com.spikes2212.command.genericsubsystem.commands;
 
 import com.spikes2212.command.genericsubsystem.TalonSubsystem;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import java.util.function.Supplier;
@@ -20,20 +19,14 @@ public class MoveTalonSubsystem extends CommandBase {
      */
     private final Supplier<Double> setpoint;
 
-    private final Supplier<Double> waitTime;
-    private double lastTimeNotOnTarget;
-
-
-    public MoveTalonSubsystem(TalonSubsystem subsystem, Supplier<Double> setpoint, Supplier<Double> waitTime) {
+    public MoveTalonSubsystem(TalonSubsystem subsystem, Supplier<Double> setpoint) {
         addRequirements(subsystem);
         this.subsystem = subsystem;
         this.setpoint = setpoint;
-        this.waitTime = waitTime;
-        this.lastTimeNotOnTarget = 0;
     }
 
-    public MoveTalonSubsystem(TalonSubsystem subsystem, double setpoint, Supplier<Double> waitTime) {
-        this(subsystem, () -> setpoint, waitTime);
+    public MoveTalonSubsystem(TalonSubsystem subsystem, double setpoint) {
+        this(subsystem, () -> setpoint);
     }
 
     @Override
@@ -53,10 +46,6 @@ public class MoveTalonSubsystem extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        if(!subsystem.onTarget(setpoint.get())) {
-            lastTimeNotOnTarget = Timer.getFPGATimestamp();
-        }
-
-        return subsystem.onTarget(setpoint.get()) && Timer.getFPGATimestamp() - lastTimeNotOnTarget <= waitTime.get();
+        return subsystem.onTarget(setpoint.get());
     }
 }
