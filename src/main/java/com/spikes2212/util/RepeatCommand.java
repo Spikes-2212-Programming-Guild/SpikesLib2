145705e4d8e74ded06aab9c73e6ab0e5527acd75
Ergoold <eran1gold@gmail.com>
2020-02-21 09:43:13 +0200

    add `RepeatCommand`

---
+++ src/main/java/com/spikes2212/util/RepeatCommand.java
@@ -0,0 +1,34 @@
+package com.spikes2212.util;
+
+import edu.wpi.first.wpilibj2.command.*;
+
+/**
+ * This {@link Command} repeats a sequence of commands indefinitely
+ */
+
+public class RepeatCommand extends CommandBase {
+
+    private Command command;
+
+    public RepeatCommand(Command... command) {
+        this.command = new SequentialCommandGroup(command);
+    }
+
+    @Override
+    public void initialize() {
+        command.initialize();
+    }
+
+    @Override
+    public void execute() {
+        command.execute();
+        if(command.isFinished()) {
+            command.initialize();
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        command.end(interrupted);
+    }
+}
