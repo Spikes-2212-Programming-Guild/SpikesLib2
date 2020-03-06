781f2cbf448e30c21102ddb86c28e5f5a805e089
Ergoold <eran1gold@gmail.com>
2020-03-06 14:16:32 +0200

    add access to more parameters from the limelight

--- src/main/java/com/spikes2212/util/Limelight.java
+++ src/main/java/com/spikes2212/util/Limelight.java
@@ -1,8 +1,6 @@
 package com.spikes2212.util;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.*;
 
 /**
  * A wrapper and interface for the limelight device.
@@ -68,6 +66,11 @@ public class Limelight {
     private NetworkTableEntry pipeline;
 
     /**
+     * Whether the limelight has any valid targets.
+     */
+    private NetworkTableEntry tv;
+
+    /**
      * The horizontal angle to the target the limelight sees.
      */
     private NetworkTableEntry tx;
@@ -82,6 +85,38 @@ public class Limelight {
      */
     private NetworkTableEntry ta;
 
+    /**
+     * The skew or rotation.
+     */
+    private NetworkTableEntry ts;
+
+    /**
+     * The pipeline's latency contribution.
+     */
+    private NetworkTableEntry tl;
+
+    /**
+     * Side length of the shortest side of the fitted bounding box.
+     */
+    private NetworkTableEntry tshort;
+
+    /**
+     * Side length of the longest side of the fitted bounding box.
+     */
+    private NetworkTableEntry tlong;
+
+    /**
+     * Horizontal side length of the rough bounding box.
+     * <p>
+     * Not the god of thunder.
+     */
+    private NetworkTableEntry thor;
+
+    /**
+     * Vertical side length of the rough bounding box.
+     */
+    private NetworkTableEntry tvert;
+
     public Limelight(String limelightName) {
 
         networkTable = NetworkTableInstance.getDefault().getTable(limelightName);
@@ -90,9 +125,16 @@ public class Limelight {
         camMode = networkTable.getEntry("camMode");
         pipeline = networkTable.getEntry("pipeline");
 
+        tv = networkTable.getEntry("tv");
         tx = networkTable.getEntry("tx");
         ty = networkTable.getEntry("ty");
         ta = networkTable.getEntry("ta");
+        ts = networkTable.getEntry("ts");
+        tl = networkTable.getEntry("tl");
+        tshort = networkTable.getEntry("tshort");
+        tlong = networkTable.getEntry("tlong");
+        thor = networkTable.getEntry("thor");
+        tvert = networkTable.getEntry("tvert");
     }
 
     public Limelight() {
@@ -111,6 +153,10 @@ public class Limelight {
         pipeline.setNumber(pipelineNum);
     }
 
+    public boolean getHasValidTargets() {
+        return tv.getDouble(0) == 1;
+    }
+
     public double getHorizontalAngleToTarget() {
         return tx.getDouble(0);
     }
@@ -121,6 +167,30 @@ public class Limelight {
 
     public double getTargetArea() {
         return ta.getDouble(0);
+    }
+
+    public double getSkew() {
+        return ts.getDouble(0);
+    }
+
+    public double getPipelineLatency() {
+        return tl.getDouble(0);
+    }
+
+    public double getShortestFittedBoundingBoxSide() {
+        return tshort.getDouble(0);
+    }
+
+    public double getLongestFittedBoundingBoxSide() {
+        return tlong.getDouble(0);
+    }
+
+    public double getRoughBoundingBoxHorizontalSide() {
+        return thor.getDouble(0);
+    }
+
+    public double getRoughBoundingBoxVerticalSide() {
+        return tvert.getDouble(0);
     }
 
 }
