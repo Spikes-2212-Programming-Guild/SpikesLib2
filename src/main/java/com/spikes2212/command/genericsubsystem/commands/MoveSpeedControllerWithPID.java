23da399eb8427703bdb6f632834b3228db05fbaf
Ergoold <eran1gold@gmail.com>
2020-08-12 09:31:42 +0300

    modify `MoveTalonWithPID` to use the `PIDSpeedController` interface and rename it to `MoveSpeedControllerWithPID`

--- src/main/java/com/spikes2212/command/genericsubsystem/commands/MoveSpeedControllerWithPID.java
+++ src/main/java/com/spikes2212/command/genericsubsystem/commands/MoveSpeedControllerWithPID.java
@@ -2,6 +2,7 @@ package com.spikes2212.command.genericsubsystem.commands;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.spikes2212.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.control.PIDSpeedController;
 import com.spikes2212.control.PIDTalon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -11,31 +12,30 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**
- * This command makes a Talon speed controller run a PID loop.
+ * This command makes a {@link PIDSpeedController} run a PID loop.
  */
-public class MoveTalonWithPID extends CommandBase {
+public class MoveSpeedControllerWithPID extends CommandBase {
 
     /**
-     * The {@link PIDTalon} with the necessary details.
+     * The {@link PIDSpeedController} with the necessary details.
      */
-    private PIDTalon talon;
+    private PIDSpeedController speedController;
+
     /**
      * A {@link Supplier} that returns the maximum speed allowed for the PID loop.
      */
     private Supplier<Double> maxSpeed;
+
     /**
      * A {@link Supplier} that returns the minimum speed allowed for the PID loop.
      */
     private Supplier<Double> minSpeed;
+
     /**
      * A {@link Supplier} that returns the target setpoint for the PID loop.
      */
     private Supplier<Double> setpoint;
-    /**
-     * The Talon's timeout. Any process that changes the Talon's configurations will time out after that many ms.
-     * Use 0 if you want to have no time limit on such processes.
-     */
-    private int timeout;
+
     /**
      * A {@link Predicate} that receives the speed of the motor and
      * returns whether that speed is valid for that motor.
@@ -43,94 +43,88 @@ public class MoveTalonWithPID extends CommandBase {
     private Predicate<Double> canMove;
     private double lastTimeNotOnTarget;
 
-    public MoveTalonWithPID(Subsystem subsystem, PIDTalon talon, Supplier<Double> maxSpeed,
-                            Supplier<Double> minSpeed, ControlMode mode, Supplier<Double> setpoint,
-                            int timeout, Predicate<Double> canMove) {
+    public MoveSpeedControllerWithPID(Subsystem subsystem, PIDTalon speedController, Supplier<Double> maxSpeed,
+                                      Supplier<Double> minSpeed, ControlMode mode, Supplier<Double> setpoint,
+                                      Predicate<Double> canMove) {
         addRequirements(subsystem);
-        this.talon = talon;
+        this.speedController = speedController;
         this.maxSpeed = maxSpeed;
         this.minSpeed = minSpeed;
         this.setpoint = setpoint;
-        this.timeout = timeout;
         this.canMove = canMove;
     }
 
-    public MoveTalonWithPID(GenericSubsystem genericSubsystem, PIDTalon talon, ControlMode mode,
-                            Supplier<Double> setpoint, int timeout) {
+    public MoveSpeedControllerWithPID(GenericSubsystem genericSubsystem, PIDTalon speedController, ControlMode mode,
+                                      Supplier<Double> setpoint) {
         addRequirements(genericSubsystem);
-        this.talon = talon;
+        this.speedController = speedController;
         this.maxSpeed = genericSubsystem.maxSpeed;
         this.minSpeed = genericSubsystem.minSpeed;
         this.setpoint = setpoint;
-        this.timeout = timeout;
         this.canMove = genericSubsystem::canMove;
     }
 
-    public MoveTalonWithPID(Subsystem subsystem, PIDTalon talon, Supplier<Double> maxSpeed,
-                            Supplier<Double> minSpeed, ControlMode mode, Supplier<Double> setpoint, int timeout) {
+    public MoveSpeedControllerWithPID(Subsystem subsystem, PIDTalon speedController, Supplier<Double> maxSpeed,
+                                      Supplier<Double> minSpeed, ControlMode mode, Supplier<Double> setpoint) {
         addRequirements(subsystem);
-        this.talon = talon;
+        this.speedController = speedController;
         this.maxSpeed = maxSpeed;
         this.minSpeed = minSpeed;
         this.setpoint = setpoint;
-        this.timeout = timeout;
         this.canMove = v -> true;
     }
 
-    public MoveTalonWithPID(Subsystem subsystem, PIDTalon talon, Supplier<Double> maxSpeed,
-                            Supplier<Double> minSpeed, Supplier<Double> setpoint, int timeout, Predicate<Double> canMove) {
+    public MoveSpeedControllerWithPID(Subsystem subsystem, PIDTalon speedController, Supplier<Double> maxSpeed,
+                                      Supplier<Double> minSpeed, Supplier<Double> setpoint, Predicate<Double> canMove) {
         addRequirements(subsystem);
-        this.talon = talon;
+        this.speedController = speedController;
         this.maxSpeed = maxSpeed;
         this.minSpeed = minSpeed;
         this.setpoint = setpoint;
-        this.timeout = timeout;
         this.canMove = canMove;
     }
 
-    public MoveTalonWithPID(GenericSubsystem genericSubsystem, PIDTalon talon, Supplier<Double> setpoint, int timeout) {
+    public MoveSpeedControllerWithPID(GenericSubsystem genericSubsystem, PIDTalon speedController, Supplier<Double> setpoint) {
         addRequirements(genericSubsystem);
-        this.talon = talon;
+        this.speedController = speedController;
         this.maxSpeed = genericSubsystem.maxSpeed;
         this.minSpeed = genericSubsystem.minSpeed;
         this.setpoint = setpoint;
-        this.timeout = timeout;
         this.canMove = genericSubsystem::canMove;
     }
 
-    public MoveTalonWithPID(Subsystem subsystem, PIDTalon talon, Supplier<Double> maxSpeed,
-                            Supplier<Double> minSpeed, Supplier<Double> setpoint, int timeout) {
+    public MoveSpeedControllerWithPID(Subsystem subsystem, PIDTalon speedController, Supplier<Double> maxSpeed,
+                                      Supplier<Double> minSpeed, Supplier<Double> setpoint) {
         addRequirements(subsystem);
-        this.talon = talon;
+        this.speedController = speedController;
         this.maxSpeed = maxSpeed;
         this.minSpeed = minSpeed;
         this.setpoint = setpoint;
-        this.timeout = timeout;
         this.canMove = v -> true;
     }
 
     @Override
     public void initialize() {
-        talon.configureLoop(maxSpeed, minSpeed, timeout);
+        speedController.configureLoop(maxSpeed, minSpeed);
     }
 
     @Override
     public void execute() {
-        talon.pidSet(setpoint.get(), timeout);
+        speedController.pidSet(setpoint.get());
     }
 
     @Override
     public void end(boolean interrupted) {
-        talon.finish();
+        speedController.finish();
     }
 
     @Override
     public boolean isFinished() {
-        if (!talon.onTarget(setpoint.get())) {
+        if (!speedController.onTarget(setpoint.get())) {
             lastTimeNotOnTarget = Timer.getFPGATimestamp();
         }
 
-        return !canMove.test(talon.getTalon().getMotorOutputPercent()) ||
-                Timer.getFPGATimestamp() - lastTimeNotOnTarget >= talon.getSettings().getWaitTime();
+        return !canMove.test(speedController.getSpeed()) ||
+                Timer.getFPGATimestamp() - lastTimeNotOnTarget >= speedController.getWaitTime();
     }
 }
