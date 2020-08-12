3642616c665b0b7adc69986c5b64cbd1a6ca72f0
Ergoold <eran1gold@gmail.com>
2020-08-12 09:13:50 +0300

    make `PIDTalon` implement `PIDSpeedController`

--- src/main/java/com/spikes2212/control/PIDTalon.java
+++ src/main/java/com/spikes2212/control/PIDTalon.java
@@ -3,53 +3,59 @@ package com.spikes2212.control;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.spikes2212.control.PIDSettings;
 
 import java.util.function.Supplier;
 
-public class PIDTalon {
+public class PIDTalon implements PIDSpeedController {
 
     /**
      * The {@link WPI_TalonSRX} on which the PID is calculated.
      */
-    private WPI_TalonSRX talon;
+    private final WPI_TalonSRX talon;
+
     /**
      * The PID loop's {@link PIDSettings}.
      */
-    private PIDSettings settings;
+    private final PIDSettings settings;
+
     /**
      * The PID loop's feed forward constant.
      */
-    private Supplier<Double> kF;
+    private final Supplier<Double> kF;
+
     /**
      * The PID loop's {@link ControlMode}.
      */
-    private ControlMode mode;
+    private final ControlMode mode;
+
+    /**
+     * The talon motor's timeout.
+     */
+    private int timeout;
 
     /**
      * Constructs a PIDTalon instance with the given parameters as field values.
      *
-     * @param talon The Talon speed controller on which the PID loop is calculated.
+     * @param talon    The Talon speed controller on which the PID loop is calculated.
      * @param settings The PID loop's {@link PIDSettings}.
-     * @param kF The PID loop's feed forward constant.
-     * @param mode The PID loop's {@link ControlMode}.
+     * @param kF       The PID loop's feed forward constant.
+     * @param mode     The PID loop's {@link ControlMode}.
      */
-    public PIDTalon(WPI_TalonSRX talon, PIDSettings settings, Supplier<Double> kF, ControlMode mode) {
+    public PIDTalon(WPI_TalonSRX talon, PIDSettings settings, Supplier<Double> kF, ControlMode mode, int timeout) {
         this.talon = talon;
         this.settings = settings;
-        this.kF= kF;
+        this.kF = kF;
         this.mode = mode;
+        this.timeout = timeout;
     }
 
     /**
      * Configures the Talon speed controller's PID constants as well as others that help maintain safety.
+     *
      * @param maxSpeed A {@link Supplier} that returns the maximum speed allowed for the PID loop.
      * @param minSpeed A {@link Supplier} that returns the minimum speed allowed for the PID loop.
-     * @param timeout The Talon's timeout.
-     *                Any process that changes the Talon's configurations will time out after that many ms.
-     *                Use 0 if you want to have no time limit on such processes.
      */
-    public void configureLoop(Supplier<Double> maxSpeed, Supplier<Double> minSpeed, int timeout) {
+    public void configureLoop(Supplier<Double> maxSpeed, Supplier<Double> minSpeed) {
         talon.configFactoryDefault();
         talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, timeout);
 
@@ -67,12 +73,10 @@ public class PIDTalon {
 
     /**
      * Sets the Talon speed controller to move as dictated by the PID calculation.
+     *
      * @param setpoint The PID loop's setpoint.
-     * @param timeout The Talon's timeout.
-     *                Any process that changes the Talon's configurations will time out after that many ms.
-     *                Use 0 if you want to have no time limit on such processes.
      */
-    public void pidSet(double setpoint, int timeout) {
+    public void pidSet(double setpoint) {
         talon.config_kP(0, settings.getkP(), timeout);
         talon.config_kI(0, settings.getkI(), timeout);
         talon.config_kD(0, settings.getkD(), timeout);
@@ -89,6 +93,7 @@ public class PIDTalon {
 
     /**
      * Returns whether the PID loop is close enough to the target setpoint.
+     *
      * @param setpoint The target setpoint.
      * @return Whether the PID loop is close enough to the target setpoint.
      */
@@ -98,6 +103,7 @@ public class PIDTalon {
 
     /**
      * Returns the {@link WPI_TalonSRX} on which the PID is calculated.
+     *
      * @return The {@link WPI_TalonSRX} on which the PID is calculated.
      */
     public WPI_TalonSRX getTalon() {
@@ -106,6 +112,7 @@ public class PIDTalon {
 
     /**
      * Returns the PID loop's {@link PIDSettings}.
+     *
      * @return The PID loop's {@link PIDSettings}.
      */
     public PIDSettings getSettings() {
