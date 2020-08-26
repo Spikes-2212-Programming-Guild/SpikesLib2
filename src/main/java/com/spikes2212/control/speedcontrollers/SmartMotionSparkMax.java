f5d4ba1a147d381a80e5fc21b6eca6dcb44d6edb
Ergoold <eran1gold@gmail.com>
2020-08-26 11:46:38 +0300

    document `SmartMotionSparkMax`

--- src/main/java/com/spikes2212/control/speedcontrollers/SmartMotionSparkMax.java
+++ src/main/java/com/spikes2212/control/speedcontrollers/SmartMotionSparkMax.java
@@ -7,10 +7,26 @@ import com.spikes2212.control.SmartMotionSettings;
 
 import java.util.function.Supplier;
 
+/**
+ * A {@link CANSparkMax} that can run a PID loop.
+ *
+ * @author Ergoold
+ */
 public class SmartMotionSparkMax extends PIDSparkMax {
 
+    /**
+     * The smart motion loop's {@link SmartMotionSettings}.
+     */
     private final SmartMotionSettings motionSettings;
 
+    /**
+     * Constructs a {@code SmartMotionSparkMax} with the given parameters as field values.
+     *
+     * @param sparkMax       the Spark Max speed controller on which the PID loop is calculated
+     * @param settings       the PID loop's {@link PIDSettings}
+     * @param motionSettings the smart motion loop's {@link SmartMotionSettings}
+     * @param kF             the PID loop's feed forward constant
+     */
     public SmartMotionSparkMax(CANSparkMax sparkMax, PIDSettings settings, SmartMotionSettings motionSettings,
                                Supplier<Double> kF, int timeout) {
         super(sparkMax, settings, kF, ControlType.kVelocity, timeout);
