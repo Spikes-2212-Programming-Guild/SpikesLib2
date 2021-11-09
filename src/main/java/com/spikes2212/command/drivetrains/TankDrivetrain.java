12a481b4af4211bda8dc66baf38d8ebd8ff45eb1
OfriRosenbaum <ofriro1000>
2021-11-09 19:38:29 +0200

    TankDrivetrain conventions fixes

--- src/main/java/com/spikes2212/command/drivetrains/TankDrivetrain.java
+++ src/main/java/com/spikes2212/command/drivetrains/TankDrivetrain.java
@@ -8,9 +8,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * This class represents a type of Drivetrain that its left and right sides are
  * controlled independently, allowing it to move by giving each side a speed
  * value separately.
- * <p>
- * It can move forwards/backwards by giving both its sides an equal speed or
- * turn by giving the sides different speeds
+ *
+ * <p> It can move forwards/backwards by giving both its sides an equal speed or
+ * turn by giving the sides different speeds </p>
  *
  * @author Yuval Levy
  */
@@ -79,8 +79,7 @@ public class TankDrivetrain extends SubsystemBase {
     /**
      * Moves the left side of this drivetrain by a given speed.
      *
-     * @param speedLeft the speed to set to the left side. Positive values move this side
-     *                  forward.
+     * @param speedLeft the speed to set to the left side. Positive values move this side forward.
      */
     public void setLeft(double speedLeft) {
         leftController.set(speedLeft);
@@ -89,8 +88,7 @@ public class TankDrivetrain extends SubsystemBase {
     /**
      * Moves the right side of this drivetrain with a given speed.
      *
-     * @param speedRight the speed to set to the right side. Positive values move this side
-     *                   forward.
+     * @param speedRight the speed to set to the right side. Positive values move this side forward.
      */
     public void setRight(double speedRight) {
         rightController.set(-speedRight);
