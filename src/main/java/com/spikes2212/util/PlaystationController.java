f39d732df40653f72629118b4dd645ce0d5e23ec
OfriRosenbaum <ofriro1000>
2021-12-16 19:40:20 +0200

    added PlaystationController class for PS4 Controller

---
+++ src/main/java/com/spikes2212/util/PlaystationController.java
@@ -0,0 +1,186 @@
+package com.spikes2212.util;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * A class that handles the communication between an PS4 controller and the code
+ *
+ * @author Ofri Rosenbaum
+ */
+public class PlaystationController extends Joystick {
+
+    private PS4Controller ps;
+
+    /**
+     * Constructs a new {@link PS4Controller} using the port of the USB on the driver
+     * station.
+     *
+     * @param port The port on the Driver Station that the joystick is plugged into.
+     */
+    public PlaystationController(int port) {
+        super(port);
+    }
+
+
+    public JoystickButton getCrossButton() {
+        return new JoystickButton(this, PS4Controller.Button.kCross.value);
+    }
+
+    public JoystickButton getSquareButton() {
+        return new JoystickButton(this, PS4Controller.Button.kSquare.value);
+    }
+
+    public JoystickButton getCircleButton() {
+        return new JoystickButton(this, PS4Controller.Button.kCircle.value);
+    }
+
+    public JoystickButton getTriangleButton() {
+        return new JoystickButton(this, PS4Controller.Button.kTriangle.value);
+    }
+
+    public JoystickButton getButtonOptions() {
+        return new JoystickButton(this, PS4Controller.Button.kOptions.value);
+    }
+
+    public JoystickButton getButtonShare() {
+        return new JoystickButton(this, PS4Controller.Button.kShare.value);
+    }
+
+    public double getR2Axis() {
+        return ps.getR2Axis();
+    }
+
+    public double getL2Axis() {
+        return ps.getL2Axis();
+    }
+
+    public Trigger getR2Button() {
+        return new Trigger() {
+
+            @Override
+            public boolean get() {
+                return ps.getR2Button();
+            }
+        };
+    }
+
+    public Trigger getL2Button() {
+        return new Trigger() {
+
+            @Override
+            public boolean get() {
+                return ps.getL2Button();
+            }
+        };
+    }
+
+    public JoystickButton getR1Button() {
+        return new JoystickButton(this, PS4Controller.Button.kR1.value);
+    }
+
+    public JoystickButton getL1Button() {
+        return new JoystickButton(this, PS4Controller.Button.kL1.value);
+    }
+
+    public JoystickButton getRightStickButton() {
+        return new JoystickButton(this, PS4Controller.Button.kR3.value);
+    }
+
+    public JoystickButton getLeftStickButton() {
+        return new JoystickButton(this, PS4Controller.Button.kL3.value);
+    }
+
+    public double getRightX() {
+        return ps.getRightX();
+    }
+
+    public double getRightY() {
+        return ps.getRightY();
+    }
+
+    public double getLeftX() {
+        return ps.getLeftX();
+    }
+
+    public double getLeftY() {
+        return ps.getLeftY();
+    }
+
+    public Button getUpButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 0;
+            }
+        };
+    }
+
+    public Button getDownButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 180;
+            }
+        };
+    }
+
+
+    public Button getLeftButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 270;
+            }
+        };
+    }
+
+
+    public Button getRightButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 90;
+            }
+        };
+    }
+
+    public Button getUpperRightButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 45;
+            }
+        };
+    }
+
+    public Button getLowerRightButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 135;
+            }
+        };
+    }
+
+    public Button getLowerLeftButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 225;
+            }
+        };
+    }
+
+    public Button getUpperLeftButton() {
+        return new Button() {
+            @Override
+            public boolean get() {
+                return getPOV() == 315;
+            }
+        };
+    }
+}
