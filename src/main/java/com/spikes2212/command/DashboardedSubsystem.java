48bcfa62f7156d34a6765c712767eba372685d8a
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-04-06 17:48:29 +0300

    removed interfface

--- src/main/java/com/spikes2212/command/DashboardedSubsystem.java
+++ src/main/java/com/spikes2212/command/DashboardedSubsystem.java
@@ -2,14 +2,13 @@ package com.spikes2212.command;
 
 import com.spikes2212.dashboard.Namespace;
 import com.spikes2212.dashboard.RootNamespace;
-import com.spikes2212.util.Dashboarded;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * A {@link Subsystem} that includes a {@link Namespace}.
  */
-public abstract class DashboardedSubsystem extends SubsystemBase implements Dashboarded {
+public abstract class DashboardedSubsystem extends SubsystemBase {
 
     protected RootNamespace rootNamespace;
 
@@ -18,18 +17,12 @@ public abstract class DashboardedSubsystem extends SubsystemBase implements Dash
     }
 
     /**
-     * @return the subsystem's {@link #rootNamespace}
-     */
-    @Override
-    public RootNamespace getRootNamespace() {
-        return rootNamespace;
-    }
-
-    /**
      * Updates the {@link Namespace}. Should be called in the {@code robotPeriodic()} method in {@code Robot}.
      */
     @Override
     public void periodic() {
-        Dashboarded.super.periodic();
+        rootNamespace.update();
     }
+
+    public abstract void configureDashboard();
 }
