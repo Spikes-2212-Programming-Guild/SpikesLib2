581f12e46204c0bb3c7fb849cfe9d1423e827e22
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-04-06 14:51:02 +0300

    made PigeonWrapper implement gyro

--- src/main/java/com/spikes2212/util/PigeonWrapper.java
+++ src/main/java/com/spikes2212/util/PigeonWrapper.java
@@ -2,16 +2,21 @@ package com.spikes2212.util;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.sensors.PigeonIMU;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
  * A wrapper class for the pigeon IMU sensor.
  *
  * @author Tal Sitton
  */
-public class PigeonWrapper {
+public class PigeonWrapper implements Gyro {
 
     public double[] values = new double[3];
     protected final PigeonIMU pigeon;
+
+    private double lastTime = 0;
+    private double lastAngle;
 
     public PigeonWrapper(int canPort) {
         pigeon = new PigeonIMU(canPort);
@@ -26,6 +31,24 @@ public class PigeonWrapper {
      */
     public void reset() {
         setYaw(0);
+    }
+
+    @Override
+    public double getAngle() {
+        return getYaw();
+    }
+
+    /**
+     * @return the yaw change rate in degrees per second
+     */
+    @Override
+    public double getRate() {
+        double currentTime = Timer.getFPGATimestamp() / 1000;
+        double currentAngle = getYaw();
+        double rate = (currentAngle - lastAngle) / (currentTime - lastTime);
+        lastAngle = currentAngle;
+        lastTime = currentTime;
+        return rate;
     }
 
     /**
@@ -84,5 +107,13 @@ public class PigeonWrapper {
      */
     public void setYaw(double yaw) {
         pigeon.setYaw(yaw);
+    }
+
+    /**
+     * Don't.
+     */
+    @Override
+    public void close() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Why did you feel the need to close the pigeon");
     }
 }
