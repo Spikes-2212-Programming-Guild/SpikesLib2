d48b157b6bb8d40ae2b71dc4cf8813150ba43b11
Yoel-Perman-Brilliant <yoyoperman@gmail.com>
2022-05-20 22:20:56 +0300

    added deprecated constructor

--- src/main/java/com/spikes2212/command/genericsubsystem/GenericSubsystem.java
+++ src/main/java/com/spikes2212/command/genericsubsystem/GenericSubsystem.java
@@ -30,10 +30,10 @@ public abstract class GenericSubsystem extends DashboardedSubsystem {
      * Constructs a new instance of GenericSubsystem. <br>
      *
      * This constructor is deprecated. <br>
-     * Please use {@code GenericSubsystem(String namespaceName)}.
+     * Please use {@link #GenericSubsystem(String namespaceName)}.
      */
 
-    @Deprecated(since = "2022")
+    @Deprecated(since = "2022", forRemoval = true)
     public GenericSubsystem() {
         this("generic subsystem");
     }
@@ -47,6 +47,21 @@ public abstract class GenericSubsystem extends DashboardedSubsystem {
      */
     public GenericSubsystem(String namespaceName, double minSpeed, double maxSpeed) {
         this(namespaceName, () -> minSpeed, () -> maxSpeed);
+    }
+
+    /**
+     * Constructs a new instance of GenericSubsystem with the given minSpeed and maxSpeed. <br><br>
+     *
+     * This constructor is deprecated. <br>
+     * Please use {@link #GenericSubsystem(String namespaceName, double minSpeed, double maxSpeed)} instead. <br><br>
+     *
+     * @param minSpeed the minimum speed
+     * @param maxSpeed the maximum speed
+     *
+     */
+    @Deprecated(since = "2022", forRemoval = true)
+    public GenericSubsystem(double minSpeed, double maxSpeed) {
+        this("generic subsystem", minSpeed, maxSpeed);
     }
 
     /**
