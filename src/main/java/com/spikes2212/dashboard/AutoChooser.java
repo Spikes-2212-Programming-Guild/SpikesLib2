f325dc3456c74ce989eb3be6196d916500ebbc4c
OfriRosenbaum <ofriro1000@gmail.com>
2022-05-20 23:07:06 +0300

    added documentation to constructor

--- src/main/java/com/spikes2212/dashboard/AutoChooser.java
+++ src/main/java/com/spikes2212/dashboard/AutoChooser.java
@@ -13,13 +13,23 @@ import java.util.HashSet;
  */
 public class AutoChooser extends SendableChooser<Command> {
 
+    /**
+     * Creates an autonomous chooser from the given commands, where the first command will be the default and the rest
+     * of the commands will be the rest of the options.
+     *
+     * <p>All options will have names matching their command's {@link Command#getName}. In case of multiple instances
+     * of the same class, numbers will also be added to differentiate between the names.</p>
+     *
+     * @param defaultOption the default command this {@link SendableChooser} will use as the default option
+     * @param options the commands that will be added to the chooser other than the default command.
+     */
     public AutoChooser(Command defaultOption, Command... options) {
         HashSet<String> names = new HashSet<>();
         String defaultName = defaultOption.getClass().getSimpleName();
         names.add(defaultName);
         setDefaultOption(defaultName, defaultOption);
         for (Command option : options) {
-            String name = option.getClass().getSimpleName();
+            String name = option.getName();
             if (names.add(name)) {
                 addOption(name, option);
             } else {
