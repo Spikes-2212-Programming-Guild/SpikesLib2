800a1d58686f040ed519a27bd082bf734d1ef580
OfriRosenbaum <ofriro1000@gmail.com>
2022-06-03 20:29:25 +0300

    fixed documentation

--- src/main/java/com/spikes2212/dashboard/AutoChooser.java
+++ src/main/java/com/spikes2212/dashboard/AutoChooser.java
@@ -68,7 +68,7 @@ public class AutoChooser extends SendableChooser<Command> {
 
     /**
      * Adds the given command with the given name as an option to this autonomous chooser.
-     * <br> In case this name already exists, a number will be added to differentiate between both options.
+     * <br> In case this name already exists, a number will be added to differentiate between the options.
      */
     @Override
     public void addOption(String name, Command command) {
