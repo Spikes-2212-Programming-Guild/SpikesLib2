1a6688c9bb4d8d771389c6be48388de665f3d33e
OfriRosenbaum <ofriro1000@gmail.com>
2022-09-19 19:08:00 +0300

    documentation fixes

--- src/main/java/com/spikes2212/dashboard/AutoChooser.java
+++ src/main/java/com/spikes2212/dashboard/AutoChooser.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import java.util.HashSet;
 
 /**
- * A class that implements {@link SendableChooser} in order to make an autonomous command chooser.
+ * A class that extends {@link SendableChooser} in order to make an autonomous command chooser.
  *
  * @author Ofri Rosenbaum
  * @see SendableChooser
@@ -29,15 +29,15 @@ public class AutoChooser extends SendableChooser<Command> {
     private final HashSet<String> names = new HashSet<>();
 
     /**
-     * Creates an {@link SendableChooser} from the given commands, where the first command will be the default option and the
+     * Creates a {@link AutoChooser} from the given commands, where the first command will be the default option and the
      * rest of the commands will be the rest of the options.
      *
      * <p>All options will have names matching their command's {@link Command#getName()}. In case of multiple
      * instances of the same class, numbers will also be added to differentiate between the names.</p>
      *
      * @param rootNamespaceName the name that will be given to the {@link RootNamespace}
-     * @param defaultOption the default command this {@link SendableChooser} will use as the default option
-     * @param options the commands that will be added to this {@link SendableChooser} other than the default command
+     * @param defaultOption the default command this {@link AutoChooser} will use as the default option
+     * @param options the commands that will be added to this {@link AutoChooser} other than the default command
      */
     public AutoChooser(String rootNamespaceName, Command defaultOption, Command... options) {
         rootNamespace = new RootNamespace(rootNamespaceName);
@@ -51,26 +51,26 @@ public class AutoChooser extends SendableChooser<Command> {
     }
 
     /**
-     * Creates an {@link SendableChooser} from the given commands, where the first command will be the default option and the
+     * Creates an {@link AutoChooser} from the given commands, where the first command will be the default option and the
      * rest of the commands will be the rest of the options.
      *
      * <p>All options will have names matching their command's {@link Command#getName()}. In case of multiple
      * instances of the same class, numbers will also be added to differentiate between the names.</p>
      *
-     * @param defaultOption the default command this {@link SendableChooser} will use as the default option
-     * @param options the commands that will be added to this {@link SendableChooser} other than the default command
+     * @param defaultOption the default command this {@link AutoChooser} will use as the default option
+     * @param options the commands that will be added to this {@link AutoChooser} other than the default command
      */
     public AutoChooser(Command defaultOption, Command... options) {
         this(DEFAULT_NAMESPACE_NAME, defaultOption, options);
     }
 
     /**
-     * Creates an {@link SendableChooser} where the first {@link Command} and {@link String} will be used as the default
+     * Creates an {@link AutoChooser} where the first {@link Command} and {@link String} will be used as the default
      * option, and the rest of the options will be parsed as following: every even index (0,2,4,...) will be a command
      * and every odd index (1,3,5,...) will be used as the name for the previous command.
      *
      * @param rootNamespaceName the name that will be given to the {@link RootNamespace}
-     * @param defaultOption the default command this {@link SendableChooser} will use as the default option
+     * @param defaultOption the default command this {@link AutoChooser} will use as the default option
      * @param defaultOptionName the name for the default command
      * @param options the rest of the options, where every even index is a command to be added and every odd index is
      *               the name for the previously mentioned command
@@ -81,11 +81,11 @@ public class AutoChooser extends SendableChooser<Command> {
     }
 
     /**
-     * Creates an {@link SendableChooser} where the first {@link Command} and {@link String} will be used as the default
+     * Creates an {@link AutoChooser} where the first {@link Command} and {@link String} will be used as the default
      * option, and the rest of the options will be parsed as following: every even index (0,2,4,...) will be a command
      * and every odd index (1,3,5,...) will be used as the name for the previous command.
      *
-     * @param defaultOption the default command this {@link SendableChooser} will use as the default option
+     * @param defaultOption the default command this {@link AutoChooser} will use as the default option
      * @param defaultOptionName the name for the default command
      * @param options the rest of the options, where every even index is a command to be added and every odd index is
      *               the name for the previous command
@@ -121,7 +121,7 @@ public class AutoChooser extends SendableChooser<Command> {
     }
 
     /**
-     * Adds the given command with the given name as an option to this {@link SendableChooser}. <br>
+     * Adds the given command with the given name as an option to this {@link AutoChooser}. <br>
      * In case this name already exists, a number will be added to differentiate between the options.
      */
     @Override
@@ -138,7 +138,7 @@ public class AutoChooser extends SendableChooser<Command> {
     }
 
     /**
-     * Schedules the selected command and closes this {@link SendableChooser}.
+     * Schedules the selected command and closes this {@link AutoChooser}.
      */
     public void schedule() {
         getSelected().schedule();
