bb055c2d6ea03904d4736d724c65a9cbe4468f86
Programming Guild <tmp@tmp.com>
2023-07-26 11:18:29 +0300

    Documentation

--- src/main/java/com/spikes2212/command/DoubleSolenoidSubsystem.java
+++ src/main/java/com/spikes2212/command/DoubleSolenoidSubsystem.java
@@ -2,11 +2,21 @@ package com.spikes2212.command;
 
 import com.spikes2212.dashboard.Namespace;
 import com.spikes2212.dashboard.RootNamespace;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
+/**
+ * A class that represents a double solenoid.
+ *
+ * @author Camellia Lami
+ * @see DashboardedSubsystem
+ */
 public class DoubleSolenoidSubsystem extends DashboardedSubsystem {
 
+    /**
+     * Creates a {@link DoubleSolenoid}.
+     */
     private final DoubleSolenoid doubleSolenoid;
 
     public DoubleSolenoidSubsystem(Namespace namespace, DoubleSolenoid doubleSolenoid) {
@@ -18,16 +28,28 @@ public class DoubleSolenoidSubsystem extends DashboardedSubsystem {
         this(new RootNamespace(namespaceName), doubleSolenoid);
     }
 
+    /**
+     * Opens the double solenoid.
+     *
+     * @return An {@link InstantCommand} that opens the solenoid.
+     */
     public InstantCommand openSolenoid() {
         return new InstantCommand(() -> doubleSolenoid.set(DoubleSolenoid.Value.kReverse));
     }
 
+    /**
+     * Closes the double solenoid.
+     *
+     * @return An {@link InstantCommand} that closes the solenoid.
+     */
     public InstantCommand closeSolenoid() {
         return new InstantCommand(() -> doubleSolenoid.set(DoubleSolenoid.Value.kForward));
     }
 
+    /**
+     * Adds any commands or data from this subsystem to the {@link NetworkTable}s.
+     */
     @Override
     public void configureDashboard() {
-
     }
 }
