d0c64c36ebe288b26706c292b0798fab4d784c33
tzintzenet <mmicroshit@outlook.com>
2023-10-16 18:52:17 +0300

    Created class SpikesLogger

---
+++ src/main/java/com/spikes2212/dashboard/SpikesLogger.java
@@ -0,0 +1,19 @@
+package com.spikes2212.dashboard;
+
+public class SpikesLogger extends RootNamespace{
+
+
+    private String key;
+    public SpikesLogger() {
+        this("SpikesLogger", "Value");
+    }
+
+    public SpikesLogger(String name, String key) {
+        super(name);
+        this.key = key;
+    }
+
+    public <T> void log(T output){
+        putString(key, output.toString());
+    }
+}
