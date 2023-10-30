f1fa700d9f337218ab62d07d1e85e8ca9417ed18
yo12344444444444 <100028795+yo12344444444444@users.noreply.github.com>
2023-10-30 19:15:57 +0200

    setVolteg

--- src/main/java/com/spikes2212/command/genericsubsystem/commands/MoveMotorGenericSubsystemWithVoltage.java
+++ src/main/java/com/spikes2212/command/genericsubsystem/commands/MoveMotorGenericSubsystemWithVoltage.java
@@ -1,6 +1,7 @@
 package com.spikes2212.command.genericsubsystem.commands;
 
 
+import com.spikes2212.command.genericsubsystem.GenericSubsystem;
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -11,6 +12,15 @@ public class MoveMotorGenericSubsystemWithVoltage extends CommandBase {
     protected final MotoredGenericSubsystem subsystem;
     protected final Supplier<Double> voltageSupplier;
 
+    /**
+     * This constructs a new {@link MoveGenericSubsystem} command using the
+     * {@link GenericSubsystem} this command operates on and a supplier supplying the
+     * speed the {@link GenericSubsystem} should move with.
+     *
+     * @param subsystem     the {@link GenericSubsystem} this command should move.
+     * @param voltageSupplier a Double {@link Supplier} supplying the voltage this subsystem
+     *                      should be moved with. Must only supply values between -1 and 1.
+     */
     public MoveMotorGenericSubsystemWithVoltage(MotoredGenericSubsystem subsystem, Supplier<Double> voltageSupplier) {
         this.subsystem = subsystem;
         this.voltageSupplier = voltageSupplier;
@@ -18,6 +28,11 @@ public class MoveMotorGenericSubsystemWithVoltage extends CommandBase {
     public MoveMotorGenericSubsystemWithVoltage(MotoredGenericSubsystem subsystem, double voltage) {
         this(subsystem, () -> voltage);
     }
+
+    /**
+     * moves the subsystem at the given voltage
+     */
+
     @Override
     public void execute(){
         subsystem.SetVoltage(this.voltageSupplier.get() / RobotController.getBatteryVoltage());
