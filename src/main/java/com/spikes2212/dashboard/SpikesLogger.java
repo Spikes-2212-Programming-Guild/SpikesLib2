8bc362db97f143cf19aa8f7c61aad479a9389550
tzintzenet <mmicroshit@outlook.com>
2023-11-02 17:09:07 +0200

    added documentation

--- src/main/java/com/spikes2212/dashboard/SpikesLogger.java
+++ src/main/java/com/spikes2212/dashboard/SpikesLogger.java
@@ -1,18 +1,33 @@
 package com.spikes2212.dashboard;
 
+/**
+ * a logger class meant to be used with the SpikesLogger desktop app to log values from the robot to a computer in real-time
+ */
 public class SpikesLogger extends RootNamespace {
 
+    /**
+     * the output location
+     */
     private final String key;
 
+    /**
+     * custom SpikesLogger with custom name and key for the output location
+     */
     public SpikesLogger(String name, String key) {
         super(name);
         this.key = key;
     }
 
+    /**
+     * default SpikesLogger
+     */
     public SpikesLogger() {
         this("SpikesLogger", "Value");
     }
 
+    /**
+     * get the output and put it in the SpikesLogger
+     */
     public <T> void log(T output) {
         putString(key, output == null ? "null" : output.toString());
     }
