960a9b2e007bd52e0621f9139febce1332488ff8
nettahalamish <bygneta1611@gmail.com>
2023-11-09 18:44:24 +0200

    added addPIDNamespace, added addFeedForwardNamespace.

--- src/main/java/com/spikes2212/dashboard/Namespace.java
+++ src/main/java/com/spikes2212/dashboard/Namespace.java
@@ -1,5 +1,7 @@
 package com.spikes2212.dashboard;
 
+import com.spikes2212.control.FeedForwardSettings;
+import com.spikes2212.control.PIDSettings;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -62,6 +64,23 @@ public interface Namespace {
     }
 
     boolean getBoolean(String key);
+
+    default void addPIDNamespace(String name, PIDSettings pidSettings) {
+        Namespace child = this.addChild(name + " pid");
+        Supplier<Double> kP = child.addConstantDouble("kP " + name, pidSettings.getkP());
+        Supplier<Double> kI = child.addConstantDouble("kI " + name, pidSettings.getkI());
+        Supplier<Double> kD = child.addConstantDouble("kD " + name, pidSettings.getkD());
+        Supplier<Double> tolerance = child.addConstantDouble(name + " tolerance", pidSettings.getTolerance());
+        Supplier<Double> waitTime = child.addConstantDouble(name + " wait time", pidSettings.getWaitTime());
+    }
+
+    default void addFeedForwardNamespace(String name, FeedForwardSettings feedForwardSettings) {
+        Namespace child = this.addChild(name + " feed forward");
+        Supplier<Double> kS = child.addConstantDouble("kS " + name, feedForwardSettings.getkS());
+        Supplier<Double> kV = child.addConstantDouble("kV " + name, feedForwardSettings.getkV());
+        Supplier<Double> kA = child.addConstantDouble("kA " + name, feedForwardSettings.getkA());
+        Supplier<Double> kG = child.addConstantDouble("kG " + name, feedForwardSettings.getkG());
+    }
 
     void update();
 }
