c86d079baa679198063ccf98974c8cb026bb32ad
Yoel Perman Brilliant <yoyoperman@gmail.com>
2023-11-16 14:43:04 +0200

    changed doumentation and blank lines

--- src/main/java/com/spikes2212/command/drivetrains/commands/DriveArcade.java
+++ src/main/java/com/spikes2212/command/drivetrains/commands/DriveArcade.java
@@ -19,6 +19,7 @@ public class DriveArcade extends CommandBase {
     protected final Supplier<Double> moveValueSupplier;
     protected final Supplier<Double> rotateValueSupplier;
     protected final Supplier<Boolean> isFinished;
+
     /**
      * Whether to square the velocity suppliers.
      */
@@ -29,11 +30,13 @@ public class DriveArcade extends CommandBase {
      * {@link TankDrivetrain} according to speed values from Double {@link Supplier}s
      * for linear and rotational movements.
      *
-     * @param drivetrain          the tank drivetrain this command operates on.
-     * @param moveValueSupplier   the double {@link Supplier} supplying the linear velocity. Positive values go forwards.
-     * @param rotateValueSupplier the double {@link Supplier} supplying the rotational velocity. Positive values go left.
-     * @param isFinished finishes the command.
-     * @param squareInputs whether to square the velocity suppliers.
+     * @param drivetrain          the tank drivetrain this command operates on
+     * @param moveValueSupplier   the double {@link Supplier} supplying the linear velocity (-1 to 1).
+     *                            Positive values go forwards
+     * @param rotateValueSupplier the double {@link Supplier} supplying the rotational velocity (-1 to 1).
+     *                            Positive values go left
+     * @param isFinished          when to finish the command
+     * @param squareInputs        whether to square the velocity suppliers
      */
     public DriveArcade(TankDrivetrain drivetrain, Supplier<Double> moveValueSupplier,
                        Supplier<Double> rotateValueSupplier, Supplier<Boolean> isFinished, boolean squareInputs) {
