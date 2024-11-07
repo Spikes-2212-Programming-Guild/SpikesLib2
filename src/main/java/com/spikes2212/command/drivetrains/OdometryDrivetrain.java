22cf768fa695938b72ca445fa42630bb0aa1c47d
Yuval <67711428+Docpower123@users.noreply.github.com>
2024-11-07 17:46:49 +0200

    deprecated OdometryDrivetrain

--- src/main/java/com/spikes2212/command/drivetrains/OdometryDrivetrain.java
+++ src/main/java/com/spikes2212/command/drivetrains/OdometryDrivetrain.java
@@ -3,6 +3,7 @@ package com.spikes2212.command.drivetrains;
 import com.spikes2212.path.OdometryHandler;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
+@Deprecated(since = "2025", forRemoval = true)
 public abstract class OdometryDrivetrain extends TankDrivetrain {
 
     public OdometryDrivetrain(String namespaceName, MotorController left, MotorController right) {
