5c989de8b4540c2ac47607f4c5668e0f9e00093e
turtleminecraft <schnitzelgirl69@gmail.com>
2025-01-16 11:23:44 +0200

    created SparkFlexWrapper.java

---
+++ src/main/java/com/spikes2212/util/SparkFlexWrapper.java
@@ -0,0 +1,8 @@
+package com.spikes2212.util;
+
+public class SparkFlexWrapper extends SparkBaseWrapper{
+
+    public SparkFlexWrapper(int deviceId, MotorType type) {
+        super(deviceId, type, SparkModel.SparkFlex);
+    }
+}
