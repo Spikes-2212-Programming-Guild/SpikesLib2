8edc6367af89244291daffdc62264bcfc7e1ce86
turtleminecraft <schnitzelgirl69@gmail.com>
2025-01-16 15:22:18 +0200

    deleted various classes

--- src/main/java/com/spikes2212/util/SparkMaxWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.spikes2212.util;
-
-import com.revrobotics.spark.SparkLowLevel;
-
-public class SparkMaxWrapper extends SparkBaseWrapper {
-    public SparkMaxWrapper(int deviceId, MotorType type) {
-        super(deviceId, type, SparkModel.SparkMax);
-    }
-}
