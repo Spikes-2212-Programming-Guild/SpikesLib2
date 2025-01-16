1f5da92fe8e475f985f2b3b4e9da9c841292527c
turtleminecraft <schnitzelgirl69@gmail.com>
2025-01-16 15:22:18 +0200

    deleted various classes

--- src/main/java/com/spikes2212/util/SparkFlexWrapper.java
+++
@@ -1,8 +0,0 @@
-package com.spikes2212.util;
-
-public class SparkFlexWrapper extends SparkBaseWrapper{
-
-    public SparkFlexWrapper(int deviceId, MotorType type) {
-        super(deviceId, type, SparkModel.SparkFlex);
-    }
-}
