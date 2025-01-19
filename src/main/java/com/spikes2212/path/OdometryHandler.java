dc4ab637dbf41231b9bd49c34d411c59da416f53
Spikes2212 <spikes.no2212@gmail.com>
2025-01-19 17:42:47 +0200

    made fields protected

--- src/main/java/com/spikes2212/path/OdometryHandler.java
+++ src/main/java/com/spikes2212/path/OdometryHandler.java
@@ -18,7 +18,7 @@ public class OdometryHandler {
     private Supplier<Double> leftPosition, rightPosition;
     private Supplier<Double> yaw;
     private double lastLeftPosition = 0, lastRightPosition = 0, lastYaw = 0;
-    private Pose2d pose;
+    private Pose2d pos;
 
     /**
      * creates a new {@link OdometryHandler} object, with given parameters
