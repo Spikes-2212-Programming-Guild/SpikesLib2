de3f131ab84ccb4c9f435397ffdbe8fb7e31d788
adiY666 <adi.yekutieljr@gmail.com>
2025-01-19 19:37:03 +0200

    removed the inverted

--- src/main/java/com/spikes2212/command/drivetrains/TankDrivetrain.java
+++ src/main/java/com/spikes2212/command/drivetrains/TankDrivetrain.java
@@ -141,7 +141,6 @@ public class TankDrivetrain extends DashboardedSubsystem {
      * @param rightSpeed the speed to set to the right side (-1 to 1). Positive values move this side forward
      */
     public void setRight(double rightSpeed) {
-        rightController.setInverted(true);
         rightController.set(rightSpeed);
     }
 
