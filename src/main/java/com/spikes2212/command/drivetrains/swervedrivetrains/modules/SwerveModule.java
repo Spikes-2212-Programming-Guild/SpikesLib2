c8426adf1b99af241e07afc84b466b921fb04284
Spikes2212 <spikes.no2212@gmail.com>
2025-01-19 16:53:53 +0200

    fixed setSpeed() needing limitSpeed

--- src/main/java/com/spikes2212/command/drivetrains/swervedrivetrains/modules/SwerveModule.java
+++ src/main/java/com/spikes2212/command/drivetrains/swervedrivetrains/modules/SwerveModule.java
@@ -6,33 +6,34 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
-import java.util.function.Supplier;
-
 public abstract class SwerveModule extends DashboardedSubsystem {
 
-    private static final double DEGREES_IN_ROTATIONS = 360;
-    private static final double DEGREES_TO_REVERSE = 180;
-    private static final double MAX_DEGREES_NEEDED_TO_ROTATE = 90;
+    protected static final double DEGREES_IN_ROTATIONS = 360;
+    protected static final double DEGREES_TO_REVERSE = 180;
+    protected static final double MAX_DEGREES_NEEDED_TO_ROTATE = 90;
 
-    private final MotorController driveController;
-    private final MotorController turnController;
+    protected final MotorController driveController;
+    protected final MotorController turnController;
 
-    private final double minSpeed;
+    protected final double offset;
+    protected final double minSpeed;
 
-    private final Translation2d modulePosition;
+    protected final Translation2d modulePosition;
 
     public SwerveModule(String namespaceName, MotorController driveController, MotorController turnController,
-                        double minSpeed, Translation2d modulePosition) {
+                        double offset, boolean driveInverted, boolean turnInverted, double minSpeed,
+                        Translation2d modulePosition) {
         super(namespaceName);
         this.driveController = driveController;
         this.turnController = turnController;
+        this.offset = offset;
         this.minSpeed = minSpeed;
         this.modulePosition = modulePosition;
     }
 
     public SwerveModule(String namespaceName, MotorController driveController, MotorController turnController,
-                        Translation2d modulePosition) {
-        this(namespaceName, driveController, turnController, 0, modulePosition);
+                        double offset, boolean driveInverted, boolean turnInverted, Translation2d modulePosition) {
+        this(namespaceName, driveController, turnController, offset, driveInverted, turnInverted, 0, modulePosition);
     }
 
     public void set(SwerveModuleState state, boolean usePID, boolean limitSpeed) {
@@ -41,11 +42,11 @@ public abstract class SwerveModule extends DashboardedSubsystem {
             return;
         }
         state = optimize(state);
-        setSpeed(state.speedMetersPerSecond, usePID, limitSpeed);
+        setSpeed(state.speedMetersPerSecond, usePID);
         setAngle(state.angle.getDegrees());
     }
 
-    protected abstract void setSpeed(double speed, boolean usePID, boolean limitSpeed);
+    protected abstract void setSpeed(double speed, boolean usePID);
 
     protected abstract void setAngle(double angle);
 
