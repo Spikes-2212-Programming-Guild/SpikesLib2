5fd73eeb5547b80469354f8aa947541824d7c285
Spikes2212 <spikes.no2212@gmail.com>
2025-01-21 10:19:07 +0200

    added turnPIDSettings field

--- src/main/java/com/spikes2212/command/drivetrains/swervedrivetrains/modules/SwerveModule.java
+++ src/main/java/com/spikes2212/command/drivetrains/swervedrivetrains/modules/SwerveModule.java
@@ -1,6 +1,7 @@
 package com.spikes2212.command.drivetrains.swervedrivetrains.modules;
 
 import com.spikes2212.command.DashboardedSubsystem;
+import com.spikes2212.control.PIDSettings;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
@@ -18,22 +19,27 @@ public abstract class SwerveModule extends DashboardedSubsystem {
     protected final double offset;
     protected final double minSpeed;
 
+    protected final PIDSettings turnPIDSettings;
+
     protected final Translation2d modulePosition;
 
     public SwerveModule(String namespaceName, MotorController driveController, MotorController turnController,
                         double offset, boolean driveInverted, boolean turnInverted, double minSpeed,
-                        Translation2d modulePosition) {
+                        PIDSettings turnPIDSettings, Translation2d modulePosition) {
         super(namespaceName);
         this.driveController = driveController;
         this.turnController = turnController;
         this.offset = offset;
         this.minSpeed = minSpeed;
+        this.turnPIDSettings = turnPIDSettings;
         this.modulePosition = modulePosition;
     }
 
     public SwerveModule(String namespaceName, MotorController driveController, MotorController turnController,
-                        double offset, boolean driveInverted, boolean turnInverted, Translation2d modulePosition) {
-        this(namespaceName, driveController, turnController, offset, driveInverted, turnInverted, 0, modulePosition);
+                        double offset, boolean driveInverted, boolean turnInverted, PIDSettings turnPIDSettings,
+                        Translation2d modulePosition) {
+        this(namespaceName, driveController, turnController, offset, driveInverted, turnInverted, 0, turnPIDSettings,
+                modulePosition);
     }
 
     public void set(SwerveModuleState state, boolean usePID, boolean limitSpeed) {
