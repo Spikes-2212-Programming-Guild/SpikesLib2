6bfaf073dd57436b2a61742f7af07b5142cb0a75
adiY666 <adi.yekutieljr@gmail.com>
2025-01-26 17:41:03 +0200

    Finalized fields in MotoredGenericSubsystem

--- src/main/java/com/spikes2212/command/genericsubsystem/MotoredGenericSubsystem.java
+++ src/main/java/com/spikes2212/command/genericsubsystem/MotoredGenericSubsystem.java
@@ -14,7 +14,7 @@ import java.util.function.Supplier;
  */
 public class MotoredGenericSubsystem extends GenericSubsystem {
 
-    protected MotorController motorController;
+    protected final MotorController motorController;
 
     /**
      * Constructs a new instance of {@link MotoredGenericSubsystem} with the given {@link Namespace}'s name, the given
