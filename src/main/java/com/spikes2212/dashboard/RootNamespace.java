248ba4f01639b7b67bcac69df9a57cdd8efa40c7
Spikes2212 <spikes.no2212@gmail.com>
2025-01-27 18:17:12 +0200

    bruh

--- src/main/java/com/spikes2212/dashboard/RootNamespace.java
+++ src/main/java/com/spikes2212/dashboard/RootNamespace.java
@@ -23,7 +23,7 @@ public class RootNamespace implements Namespace {
     protected final NetworkTable table;
     protected final String name;
 
-    private final Map<String, Sendable> TABLES_TO_DATA = new HashMap<>();
+    private final Map<String, Sendable> tablesToData;
 
     public RootNamespace(String name) {
         this.name = name;
@@ -32,6 +32,7 @@ public class RootNamespace implements Namespace {
         stringFields = new HashMap<>();
         numberFields = new HashMap<>();
         booleanFields = new HashMap<>();
+        tablesToData = new HashMap<>();
     }
 
     @Override
@@ -71,9 +72,9 @@ public class RootNamespace implements Namespace {
 
     @Override
     public void putData(String key, Sendable value) {
-        Sendable sddata = TABLES_TO_DATA.get(key);
+        Sendable sddata = tablesToData.get(key);
         if (sddata == null || sddata != value) {
-            TABLES_TO_DATA.put(key, value);
+            tablesToData.put(key, value);
             NetworkTable dataTable = table.getSubTable(key);
             SendableBuilderImpl builder = new SendableBuilderImpl();
             builder.setTable(dataTable);
@@ -171,7 +172,7 @@ public class RootNamespace implements Namespace {
     }
 
     private void updateSendable() {
-        for (Sendable data : TABLES_TO_DATA.values()) {
+        for (Sendable data : tablesToData.values()) {
             SendableRegistry.update(data);
         }
     }
