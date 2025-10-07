d7256729be32eefaffd6f713bb46622102c16635
GIL <eingar.gil@gmail.com>
2025-10-07 21:35:01 +0300

    added SwerveModuleHolder.java

---
+++ src/main/java/com/spikes2212/command/drivetrains/swerve/SwerveModuleHolder.java
@@ -0,0 +1,12 @@
+package com.spikes2212.command.drivetrains.swerve;
+
+public abstract class SwerveModuleHolder {
+
+    public abstract SwerveModule getFrontRight();
+
+    public abstract SwerveModule getFrontLeft();
+
+    public abstract SwerveModule getBackRight();
+
+    public abstract SwerveModule getBackLeft();
+}
