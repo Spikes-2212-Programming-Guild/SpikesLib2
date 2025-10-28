bf9511651632c2148b1d9fb6b206ac7e0a83355f
GIL <eingar.gil@gmail.com>
2025-10-28 10:24:36 +0200

    i desire speeddddddddddd

--- src/main/java/com/spikes2212/command/drivetrains/swerve/SwerveDrivetrain.java
+++ src/main/java/com/spikes2212/command/drivetrains/swerve/SwerveDrivetrain.java
@@ -20,6 +20,9 @@ public abstract class SwerveDrivetrain extends DashboardedSubsystem {
 
     protected final double trackWidth;
     protected final double trackLength;
+    /*
+        The maximum desired velocity
+     */
     protected final double maxVelocity;
     protected final SwerveDriveKinematics kinematics;
 
